507d238df20e7d1321f09701955367023d4afd19
CirJ9 <157781471+CirJ9@users.noreply.github.com>
2025-06-16 16:23:50 +0800

    BAGONG DB kASAMA YUNG KAY CARLO
    , tas may dagdag haha

--- webnutri_db.sql
+++ webnutri_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 15, 2025 at 05:27 AM
+-- Generation Time: Jun 16, 2025 at 09:39 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users_data` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(10) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -46,6 +46,44 @@ INSERT INTO `users_data` (`id`, `name`, `email`, `password`, `time_joined`, `rol
 (7, 'loyd', 'loyd@gmail.com', '$2y$10$rhQRK2AAegIEw07ocvPK9eyBAJKU78uEqv9NA7ONOfeUuLFA0JVXS', '2025-06-15 02:52:51', 'user'),
 (8, 'Cir09', 'baguisi@gmail.com', '$2y$10$ebTAyJc7bSS/X/yCuQD9xOwFJVJyt1lqeYTJi.u99u48sTS4kS4z.', '2025-06-15 03:03:05', 'user');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_goals`
+--
+
+CREATE TABLE `user_goals` (
+  `goal_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `goal_type` varchar(50) DEFAULT NULL,
+  `target_calories` int(11) DEFAULT NULL,
+  `target_protein` float DEFAULT NULL,
+  `target_carbs` float DEFAULT NULL,
+  `target_fat` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_meals`
+--
+
+CREATE TABLE `user_meals` (
+  `meal_num` int(50) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `meal_name` varchar(100) NOT NULL,
+  `meal_type` enum('Breakfast','Lunch','Dinner','Snack') NOT NULL DEFAULT 'Snack',
+  `food_name` varchar(100) NOT NULL,
+  `calories` float NOT NULL,
+  `protein` float NOT NULL,
+  `fat` float NOT NULL,
+  `carbohydrates` float NOT NULL,
+  `meal_Date` date NOT NULL,
+  `meal_Time` time NOT NULL,
+  `user_Notes` text NOT NULL,
+  `is_created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
@@ -58,6 +96,20 @@ ALTER TABLE `users_data`
   ADD UNIQUE KEY `email_unique` (`email`);
 
 --
+-- Indexes for table `user_goals`
+--
+ALTER TABLE `user_goals`
+  ADD PRIMARY KEY (`goal_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `user_meals`
+--
+ALTER TABLE `user_meals`
+  ADD PRIMARY KEY (`meal_num`),
+  ADD KEY `fk_user_id` (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -65,7 +117,36 @@ ALTER TABLE `users_data`
 -- AUTO_INCREMENT for table `users_data`
 --
 ALTER TABLE `users_data`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user_goals`
+--
+ALTER TABLE `user_goals`
+  MODIFY `goal_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_meals`
+--
+ALTER TABLE `user_meals`
+  MODIFY `meal_num` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_goals`
+--
+ALTER TABLE `user_goals`
+  ADD CONSTRAINT `user_goals_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users_data` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_meals`
+--
+ALTER TABLE `user_meals`
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users_data` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_meals_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users_data` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
