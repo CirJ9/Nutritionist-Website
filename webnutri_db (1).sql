27075c0a420b36947c9ed66695f06bff43544518
CirJ9 <157781471+CirJ9@users.noreply.github.com>
2025-06-17 08:16:50 +0800

    Add files via upload

---
+++ webnutri_db (1).sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 17, 2025 at 02:16 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webnutri_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_data`
+--
+
+CREATE TABLE `users_data` (
+  `id` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `time_joined` timestamp NOT NULL DEFAULT current_timestamp(),
+  `role` enum('user','admin') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users_data`
+--
+
+INSERT INTO `users_data` (`id`, `name`, `email`, `password`, `time_joined`, `role`) VALUES
+(1, 'Jhonric', 'jhonricbaguisi09@gmail.com', '$2y$10$ZEQIO4ys18Gj/06/pRBexeiQClK2Xi8UUC0ZhIBBDjfdncpW42dE6', '2025-06-15 02:51:24', 'admin'),
+(2, 'carlo', 'carlo@gmail.com', '$2y$10$KyhtPvxy2asl4YaHNb.0iu46MNssiK2UujfHp4.5fbRT0M4n/xVu.', '2025-06-15 02:51:24', 'user'),
+(7, 'loyd', 'loyd@gmail.com', '$2y$10$rhQRK2AAegIEw07ocvPK9eyBAJKU78uEqv9NA7ONOfeUuLFA0JVXS', '2025-06-15 02:52:51', 'user'),
+(8, 'Cir09', 'baguisi@gmail.com', '$2y$10$ebTAyJc7bSS/X/yCuQD9xOwFJVJyt1lqeYTJi.u99u48sTS4kS4z.', '2025-06-15 03:03:05', 'user'),
+(9, 'jose rizal', 'rizal@gmail.com', '$2y$10$meHet9PM2GyqjrdNvNiaPetO8Y6B9TRHt1gZ1jq.3Oc4ACuZmTkla', '2025-06-16 12:44:05', 'user'),
+(10, 'Cai', 'cai@gmail.com', '$2y$10$BMrGsLh4Xa7bMV6SaO7e8O1yJf2acxulmJHM3bGeWZxS9xk7LHu6e', '2025-06-16 13:22:06', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_goals`
+--
+
+CREATE TABLE `user_goals` (
+  `goal_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `goal_type` varchar(50) DEFAULT NULL,
+  `target_calories` int(11) DEFAULT NULL,
+  `target_protein` float DEFAULT NULL,
+  `target_carbs` float DEFAULT NULL,
+  `target_fat` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_meals`
+--
+
+CREATE TABLE `user_meals` (
+  `meal_num` int(50) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `meal_name` varchar(100) NOT NULL,
+  `meal_type` enum('Breakfast','Lunch','Dinner','Snack') NOT NULL DEFAULT 'Snack',
+  `food_name` varchar(100) NOT NULL,
+  `calories` float NOT NULL,
+  `protein` float NOT NULL,
+  `fat` float NOT NULL,
+  `carbohydrates` float NOT NULL,
+  `meal_Date` date NOT NULL,
+  `meal_Time` time NOT NULL,
+  `user_Notes` text NOT NULL,
+  `is_created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_meals`
+--
+
+INSERT INTO `user_meals` (`meal_num`, `user_id`, `meal_name`, `meal_type`, `food_name`, `calories`, `protein`, `fat`, `carbohydrates`, `meal_Date`, `meal_Time`, `user_Notes`, `is_created`) VALUES
+(29, 2, 'Burger', 'Snack', 'Big Mac', 50, 30, 18, 40, '2025-06-17', '02:53:00', 'Yummy burger', '2025-06-16 18:53:18'),
+(30, 2, 'pancake', 'Breakfast', 'magnolia', 81, 82, 83, 84, '2025-06-17', '03:09:00', 'Delicious', '2025-06-16 19:09:32');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users_data`
+--
+ALTER TABLE `users_data`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email_unique` (`email`);
+
+--
+-- Indexes for table `user_goals`
+--
+ALTER TABLE `user_goals`
+  ADD PRIMARY KEY (`goal_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `user_meals`
+--
+ALTER TABLE `user_meals`
+  ADD PRIMARY KEY (`meal_num`),
+  ADD KEY `fk_user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users_data`
+--
+ALTER TABLE `users_data`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `user_goals`
+--
+ALTER TABLE `user_goals`
+  MODIFY `goal_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_meals`
+--
+ALTER TABLE `user_meals`
+  MODIFY `meal_num` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_goals`
+--
+ALTER TABLE `user_goals`
+  ADD CONSTRAINT `user_goals_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users_data` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_meals`
+--
+ALTER TABLE `user_meals`
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users_data` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_meals_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users_data` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
